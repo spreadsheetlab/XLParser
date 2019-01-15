01e53efec644953940ffa74d48dd4ed6c3e8cae0
Roel van der Steen <roel@st2x.net>
2019-01-15 18:01:57 +0100

    Extra assertion (unit test IntersectIsAtCorrectPosition)

--- src/XLParser.Tests/FormulaAnalysisTest.cs
+++ src/XLParser.Tests/FormulaAnalysisTest.cs
@@ -134,7 +134,8 @@ namespace XLParser.Tests
         public void IntersectIsAtCorrectPosition()
         {
             var fa = new FormulaAnalyzer("SUM(A1:A3 A2:A3)");
-            var intersect = fa.AllNodes.FirstOrDefault(node => node.Token?.Terminal?.Name == "INTERSECT");
+            ParseTreeNode intersect = fa.AllNodes.FirstOrDefault(node => node.Token?.Terminal?.Name == "INTERSECT");
+            Assert.IsNotNull(intersect);
             Assert.AreEqual(9, intersect.Span.Location.Position);
         }
         #endregion
