spreadsheetlab/XLParser

commit d64bb2ec73ec386bcec6cb0e6ae8551b4724004a

Fix the span position of the intersect operators.

src/XLParser/ExcelFormulaParser.cs
@@ -45,6 +45,15 @@ namespace XLParser
         public static ParseTree ParseToTree(string input)
         {
             var tree = p.Parse(input);
+            var intersects = tree.Root.AllNodes().Where(node => node.Token?.Terminal?.Name == "INTERSECT");
+
+            foreach (ParseTreeNode intersect in intersects)
+            {
+                int newPosition = intersect.Span.Location.Position - 1;
+                var newLocation = new SourceLocation(newPosition, intersect.Span.Location.Line, newPosition);
+
+                intersect.Span = new SourceSpan(newLocation, 1);
+            }
 
             if (tree.HasErrors())
             {

src/XLParser.Tests/FormulaAnalysisTest.cs
@@ -129,6 +129,14 @@ namespace XLParser.Tests
             var functions = fa.Functions().Distinct().ToList();
             CollectionAssert.Contains(functions, "INTERSECT");
         }
+
+        [TestMethod]
+        public void IntersectIsAtCorrectPosition()
+        {
+            var fa = new FormulaAnalyzer("SUM(A1:A3 A2:A3)");
+            var intersect = fa.AllNodes.FirstOrDefault(node => node.Token?.Terminal?.Name == "INTERSECT");
+            Assert.AreEqual(9, intersect.Span.Location.Position);
+        }
         #endregion
 
         #region References()

commit 0adaa1be89084d3fdb50af34f5c81d91d4137762

INTERSECT related regression
Fix bug where we tried to use the parse result before testing if the parser was successful.

Regression from d64bb2ec73ec386bcec6cb0e6ae8551b4724004a

src/XLParser/ExcelFormulaParser.cs
@@ -45,6 +45,12 @@ namespace XLParser
         public static ParseTree ParseToTree(string input)
         {
             var tree = p.Parse(input);
+
+            if (tree.HasErrors())
+            {
+                throw new ArgumentException("Failed parsing input <<" + input + ">>");
+            }
+
             var intersects = tree.Root.AllNodes().Where(node => node.Token?.Terminal?.Name == "INTERSECT");
 
             foreach (ParseTreeNode intersect in intersects)
@@ -53,11 +59,6 @@ namespace XLParser
                 var newLocation = new SourceLocation(newPosition, intersect.Span.Location.Line, newPosition);
 
                 intersect.Span = new SourceSpan(newLocation, 1);
-            }
-
-            if (tree.HasErrors())
-            {
-                throw new ArgumentException("Failed parsing input <<" + input + ">>");
             }
 
             return tree;