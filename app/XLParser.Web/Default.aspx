edcc64b28cbe34972f878f085390e16691a120f5
Roel van der Steen <roel@st2x.net>
2020-02-22 17:04:37 +0100

    Small front-end optimizations and improvements

--- app/XLParser.Web/Default.aspx
+++ app/XLParser.Web/Default.aspx
@@ -4,75 +4,87 @@
 <head runat="server">
     <meta charset="utf-8"/>
     <title>XLParser web demo</title>
-    <link rel="stylesheet" href="xlparser-web.css" type="text/css" />
+    <link rel="stylesheet" href="xlparser-web.css" type="text/css"/>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/d3/3.5.6/d3.min.js" charset="utf-8"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
-    <!--<script src="https://d3js.org/d3.v3.js" charset="utf-8"></script>-->
-    <script src="d3vizsvg.js" type="text/javascript" defer></script>
+    <script src="d3vizsvg.js" type="text/javascript" defer>
+    </script>
 </head>
 <body>
-    <div id="borderwrapper">
-        <div id="leftoflogos">
-            <h2><a href="https://github.com/PerfectXL/XLParser">XLParser</a> web demo</h2>
-    
-            <p>
-                <table>
-                    <thead><tr><th>Formula:</th><th>Version:</th><th></th></tr></thead>
-                    <tr>
-                    <td><textarea cols="25" rows="1" id="formulainput"></textarea></td>
-                    <td>
-                        <select id="parserversionselected">
-                            <option selected value="120">1.2.0</option>
-                            <option value="114">1.1.4</option>
-                            <option value="113">1.1.3</option>
-                            <option value="112">1.1.2</option>
-                            <option value="100">1.0.0</option>
-                        </select>
-                    </td>
-                    <td>
-                        <button id="parsebutton" onclick="newTree(document.getElementById('formulainput').value, document.getElementById('parserversionselected').value)">Parse</button>
-                    </td>
-                    </tr>
-                </table>
-            </p>
-    
-            <div id="bugreport">
-                <a href="javascript:;" onclick=" javascript: if(document.getElementById('bug_explanation').style.display !== 'block') {document.getElementById('bug_explanation').style.display = 'block'} else {document.getElementById('bug_explanation').style.display = 'none'};" style="color: black; font-weight: bold;">Found a bug?</a> <br/> <br/>
-                <div id="bug_explanation" style="display: none;">
-                    Great! <a href="https://github.com/PerfectXL/XLParser/issues">Please report it as a Github issue!</a> <br/> <br/>
+<div id="borderwrapper">
+    <div id="leftoflogos">
+        <h2><a href="https://github.com/PerfectXL/XLParser">XLParser</a> web demo</h2>
 
-                    If the bug is with a specific formula/excel file, please include that too.<br/>
-                    Generally bugs in XLParser are one of the following, please include this type in the report:<br/>
-                    <ul>
-                        <li>The parser can't parse a formula that Excel accepts</li>
-                        <li>The parser parses a formula that Excel doesn't accept.</li>
-                        <li>The parser interprets a formula wrong, that is it produces a parse tree that doesn't correspond with how Excel behaves.</li>
-                        <li>There is a "normal" bug in the code around the core parser.</li>
-                    </ul>
-            
-                </div>
+        <table>
+            <colgroup>
+                <col/>
+                <col style="width: 100px;"/>
+                <col style="width: 100px;"/>
+            </colgroup>
+            <thead>
+            <tr>
+                <th style="text-align: left;">Formula:</th>
+                <th style="text-align: left;">Version:</th><th></th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <td>
+                    <textarea rows="1" id="formulainput"></textarea>
+                </td>
+                <td>
+                    <select id="parserversionselected">
+                        <option selected value="120">1.2.0</option>
+                        <option value="114">1.1.4</option>
+                        <option value="100">1.0.0</option>
+                    </select>
+                </td>
+                <td>
+                    <button id="parsebutton" onclick="newTree(document.getElementById('formulainput').value, document.getElementById('parserversionselected').value)">Parse</button>
+                </td>
+            </tr>
+            </tbody>
+        </table>
+
+        <div id="bugreport">
+            <a href="javascript:;" onclick=" javascript: if (document.getElementById('bug_explanation').style.display !== 'block') { document.getElementById('bug_explanation').style.display = 'block' } else { document.getElementById('bug_explanation').style.display = 'none' };" style="color: black; font-weight: bold;">Found a bug?</a> <br/> <br/>
+            <div id="bug_explanation" style="display: none;">
+                Great! <a href="https://github.com/PerfectXL/XLParser/issues">Please report it as a Github issue!</a> <br/> <br/>
+
+                If the bug is with a specific formula/excel file, please include that too.<br/>
+                Generally bugs in XLParser are one of the following, please include this type in the report:<br/>
+                <ul>
+                    <li>The parser can't parse a formula that Excel accepts</li>
+                    <li>The parser parses a formula that Excel doesn't accept.</li>
+                    <li>The parser interprets a formula wrong, that is it produces a parse tree that doesn't correspond with how Excel behaves.</li>
+                    <li>There is a "normal" bug in the code around the core parser.</li>
+                </ul>
+
             </div>
         </div>
-        
-        <div id="logos">
-            <a href="https://tudelft.nl"><img src="img/logo-tudelft.png" alt="TU Delft logo"/></a><br/>
-            <img src="img/logo-spreadsheet-lab.png" alt="Spreadsheet lab logo"/><br/>
-            <a href="http://www.infotron.nl/"><img src="img/logo-infotron.png" alt="Infotron logo"/></a>
-        </div> 
     </div>
-    <div id="borderwrapper2">
-        <script type="text/javascript">
-            var ua = window.navigator.userAgent;
-            if (ua.indexOf("MSIE ") > 0 // IE 10-
-             || ua.indexOf('Trident/') > 0) // // IE 11
-            {
-                document.write("<em>Note: image downloading does not work properly in Internet Explorer 11 and lower.</em><br />");
-            }
-        </script>
-        <!-- Based on https://mbostock.github.io/d3/talk/20111018/tree.html and https://gist.github.com/d3noob/8326869-->
-        <p>Parse Tree (<a id="imgdatasvg">SVG</a>, <a id="imgdatapng">PNG</a>):</p>
-        <div id="d3viz">
-        </div>
+
+    <div id="logos">
+        <a href="https://tudelft.nl">
+            <img src="img/logo-tudelft.png" alt="TU Delft logo"/>
+        </a><br/>
+        <img src="img/logo-spreadsheet-lab.png" alt="Spreadsheet lab logo"/><br/>
+        <a href="https://www.infotron.nl/">
+            <img src="img/logo-infotron.png" alt="Infotron logo"/>
+        </a>
     </div>
+</div>
+<div id="borderwrapper2">
+    <script type="text/javascript">
+        var ua = window.navigator.userAgent;
+        if (ua.indexOf("MSIE ") > 0 || ua.indexOf('Trident/') > 0) {
+            document.write(
+                "<em>Note: image downloading does not work properly in Internet Explorer 11 and lower.</em><br />");
+        }
+    </script>
+    <!-- Based on https://mbostock.github.io/d3/talk/20111018/tree.html and https://gist.github.com/d3noob/8326869-->
+    <p>Parse Tree (<a id="imgdatasvg">SVG</a>, <a id="imgdatapng">PNG</a>):</p>
+    <div id="d3viz"></div>
+</div>
 </body>
 </html>
--- app/XLParser.Web/d3vizsvg.js
+++ app/XLParser.Web/d3vizsvg.js
@@ -2,15 +2,15 @@
 var latestVersion = "114";
 
 var margin = { top: 20, right: 20, bottom: 20, left: 20 },
-        width = 500 - margin.right - margin.left,
-        height = 500 - margin.top - margin.bottom;
+    width = 500 - margin.right - margin.left,
+    height = 500 - margin.top - margin.bottom;
 
 var i;
 var tree;
 var root;
 
 var diagonal = d3.svg.diagonal()
-    .projection(function (d) { return [d.x, d.y]; });
+    .projection(function(d) { return [d.x, d.y]; });
 
 var vis;
 
@@ -22,69 +22,78 @@ function newTree(formula, version) {
     d3.select("#d3viz").html("");
 
     // Request the JSON parse tree
-    d3.json(url, function (request, json) {
-        //console.log(json)
-        //console.log(request)
-        if (json !== undefined) {
-            // Calculate the needed width and height for the image
-            var tw = treeWidth(json);
-            var th = treeHeight(json);
-            //console.log("W: " + tw + " H: " + th);
-            //console.log(json);
-            var w = Math.max(tw * 75, width);
-            var h = Math.max(10 + th * 60, height);
-            var imgw = w + margin.right + margin.left;
-            var imgh = h + margin.top + margin.bottom;
+    d3.json(url,
+        function(request, json) {
+            //console.log(json)
+            //console.log(request)
+            if (json !== undefined) {
 
-            // create a tree and its container
-            tree = d3.layout.tree().size([w, h]);
-            i = 0;
+                // Calculate the required width and height for the image
+                var tw = treeWidth(json);
+                var th = treeHeight(json);
+                //console.log("W: " + tw + " H: " + th);
+                //console.log(json);
+                var w = Math.max(tw * 75, width);
+                var h = Math.max(10 + th * 60, height);
+                var imgW = w + margin.right + margin.left;
+                var imgH = h + margin.top + margin.bottom;
 
-            var svg = d3.select("#d3viz")
-            .append("svg")
-            .attr("id", "dynamicSVGParsetree")
-            .attr("version", 1.1)
-            .attr("xmlns", "http://www.w3.org/2000/svg")
-            .attr("width", imgw)
-            .attr("height", imgh)
-            ;
+                // create a tree and its container
+                tree = d3.layout.tree().size([w, h]);
+                i = 0;
 
-            svg.append("style")
-                .attr("type", "text/css")
-                //.text("<![CDATA[\n" + svgcss + "\n]]>");
-                .text(svgcss);
+                var svg = d3.select("#d3viz")
+                    .append("svg")
+                    .attr("id", "dynamicSVGParsetree")
+                    .attr("version", 1.1)
+                    .attr("xmlns", "http://www.w3.org/2000/svg")
+                    .attr("width", imgW)
+                    .attr("height", imgH);
 
-            vis = svg.append("g")
-            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+                svg.append("style").attr("type", "text/css").text(svgCss);
 
-            // Creat the tree nodes
-            root = json;
-            update(root);
+                vis = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-            // Create downloadable images
-            generateImageData(imgw, imgh);
-        } else {
-            json = JSON.parse(request.response);
-            var msg = "<code>" + json.error;
-            if (json.message !== undefined) {
-                msg += " at line " + json.message.line + " column " + json.message.column;
+                // Create the tree nodes
+                root = json;
+                update(root);
+
+                // Create images that can be downloaded
+                generateImageData(imgW, imgH);
+
+            } else {
+
+                json = JSON.parse(request.response);
+                var msg = "<code>" + json.error;
+                if (json.message !== undefined) {
+                    msg += " at line " + json.message.line + " column " + json.message.column;
+                }
+                msg += "</code><br /><br />";
+                // Convert to entities to prevent XSS
+                msg += "Input:<br /><textarea cols=\"50\" rows=\"1\" id=\"errorformulainput\" disabled>" +
+                    json.formula.replace(/./gm, function(s) { return "&#" + s.charCodeAt(0) + ";"; }) +
+                    "</textarea><br />";
+                if (json.message !== undefined) {
+                    msg += "<textarea disabled id=\"errormessages\">" +
+                        json.message.level +
+                        " at position " +
+                        json.message.line +
+                        ":" +
+                        json.message.column +
+                        "\n" +
+                        json.message.msg +
+                        "</textarea>";
+                }
+                d3.select("#d3viz").html(msg);
+
             }
-            msg += "</code><br /> <br />";
-            // Convert to entities to prevent XSS
-            msg += "Input:<br /><textarea colw='50' rows='1' id='errorformulainput' disabled>" + json.formula.replace(/./gm, function (s) { return "&#" + s.charCodeAt(0) + ";"; }) + "</textarea><br />";
-            if (json.message !== undefined) {
-                msg += "<textarea disabled id='errormessages'>" + json.message.level + " at position " + json.message.line + ":" + json.message.column + "\n" + json.message.msg + '</textarea>';
-            }
-            d3.select("#d3viz")
-            .html(msg);
-        }
-    });
+        });
 
 }
 
 // Set the parse tree image to the default formula and enter it in the formula input field
 newTree(default_formula, latestVersion);
-d3.select('#formulainput').text(default_formula);
+d3.select("#formulainput").text(default_formula);
 
 // Create nodes in the parse tree
 function update(source) {
@@ -94,43 +103,40 @@ function update(source) {
     var links = tree.links(nodes);
 
     // Normalize for fixed-depth.
-    nodes.forEach(function (d) {
+    nodes.forEach(function(d) {
         d.y = 10 + d.depth * 60;
     });
 
     // Update the nodes…
     var node = vis.selectAll("g.node")
-        .data(nodes, function (d) { return d.id || (d.id = ++i); });
+        .data(nodes, function(d) { return d.id || (d.id = ++i); });
 
     // Enter any new nodes at the parent's previous position.
     var nodeEnter = node.enter().append("g")
         .attr("class", "node")
-        .attr("transform", function (d) {
-            return "translate(" + d.x + "," + d.y + ")";
-        })
+        .attr("transform",
+            function(d) {
+                return "translate(" + d.x + "," + d.y + ")";
+            });
 
-    nodeEnter.append("circle")
-        //.attr("r", 1e-6)
-        .attr("r", 8)
-        //.style("fill", function(d) { return d._children ? "lightsteelblue" : "#fff"; });
-        .style("fill", "#fff");
+    nodeEnter.append("circle").attr("r", 8).style("fill", "#fff");
 
     nodeEnter.append("text")
-        //.attr("x", function(d) { return d.children || d._children ? -10 : 10; })
-        .attr("y", function (d) {
-            // Put nodes without children (terminals) below, nodes with (nonterminals) above
-            return d.children || d._children ? -20 : 20;
-        })
+        .attr("y",
+            function(d) {
+                // Put nodes without children (terminals) below, nodes with (non-terminals) above
+                return d.children || d._children ? -20 : 20;
+            })
         .attr("dy", ".31em")
         .attr("text-anchor", "middle")
-        .text(function (d) { return d.name.replace("\n", "\\n"); })
+        .text(function(d) { return d.name.replace("\n", "\\n"); })
         .style("fill-opacity", 1);
 
-    // Declare the links…
+    // Declare the links...
     var link = vis.selectAll("path.link")
-        .data(links, function (d) { return d.target.id; });
+        .data(links, function(d) { return d.target.id; });
 
-    // Enter the links.
+    // Enter the links
     link.enter().insert("path", "g")
         .attr("class", "link")
         .attr("d", diagonal);
@@ -161,23 +167,24 @@ function treeHeight(node) {
 // Create a downloadable SVG and PNG image from the dynamic SVG parse tree image
 // From: http://techslides.com/save-svg-as-an-image
 function generateImageData(imgw, imgh) {
+    
     var html = d3.select("#dynamicSVGParsetree")
-          .node().parentNode.innerHTML;
+        .node().parentNode.innerHTML;
 
     //console.log(html);
     // Encode the SVG data as base64 and put it in a data: link
-    var svgsrc = 'data:image/svg+xml;base64,' + btoa(html);
-    var imgdatasvg = $('#imgdatasvg')
-    imgdatasvg.attr('crossOrigin', 'anonymous');
-    imgdatasvg.attr('href', svgsrc);
-    imgdatasvg.attr('download', "parsetree.svg");
+    var svgSrc = "data:image/svg+xml;base64," + btoa(html);
+    var imgDataSvg = $("#imgdatasvg");
+    imgDataSvg.attr("crossOrigin", "anonymous");
+    imgDataSvg.attr("href", svgSrc);
+    imgDataSvg.attr("download", "parsetree.svg");
 
     // Create a new image object from the SVG
     var image = new Image;
-    image.src = svgsrc;
-    image.onload = function () {
+    image.src = svgSrc;
+    image.onload = function() {
         // Once the image is loaded
-        var imgdatapng = $('#imgdatapng');
+        var imgDataPng = $("#imgdatapng");
         try {
             // Create a canvas element and fill it with the SVG image
             var canvas = document.createElement("canvas");
@@ -188,37 +195,37 @@ function generateImageData(imgw, imgh) {
             canvasctx.drawImage(image, 0, 0);
             // Get the base64 encoded data URL for a PNG image from the canvas
             var pngsrc = canvas.toDataURL("image/png");
-            
+
             // Put it in a link
-            imgdatapng.attr('href', pngsrc);
-            imgdatapng.attr('download', "parsetree.png");
+            imgDataPng.attr("href", pngsrc);
+            imgDataPng.attr("download", "parsetree.png");
+        } catch (e) {
+            imgDataPng.attr("href", "javascript: void(0)");
+            imgDataPng.off("click");
+            imgDataPng.on("click",
+                function() {
+                    alert("An error occured while creating PNG.\n\n" +
+                        "If you are using a modern browser? This page might not have enough privileges to allow PNG creation. Increase trust level for this page.\n\n" +
+                        "Are you using an older browser? If so try a newer one.\n\n" +
+                        "Confirmed to work in Firefox 39 and Chrome 44.");
+                    return false;
+                });
         }
-        catch(e) {
-            imgdatapng.attr('href', "javascript: void(0)");
-            imgdatapng.off('click');
-            imgdatapng.on('click', function() { 
-                alert("An error occured while creating PNG.\n\n" +
-                    "If you are using a modern browser? This page might not have enough privileges to allow PNG creation. Increase trust level for this page.\n\n" +
-                    "Are you using an older browser? If so try a newer one.\n\n" +
-                    "Confirmed to work in Firefox 39 and Chrome 44.");
-                return false;
-            });
-        }
-    }
+    };
 
 };
 
-var svgcss = ".node circle {\n"+
-"    fill: #fff;\n"+
-"    stroke: steelblue;\n"+
-"    stroke-width: 1.5px;\n"+
-"}\n"+
-".node text {\n" +
-"   font-family: 'Helvetica Neue', Helvetica, sans-serif;"+
-"   font-size: 14px;\n"+
-"}\n"+
-"path.link {\n"+
-"    fill: none;\n"+
-"    stroke: #cfcfcf;\n"+
-"    stroke-width: 1.5px;\n"+
-"}";
+var svgCss = ".node circle {\n" +
+    "    fill: #fff;\n" +
+    "    stroke: steelblue;\n" +
+    "    stroke-width: 1.5px;\n" +
+    "}\n" +
+    ".node text {\n" +
+    "   font-family: 'Helvetica Neue', Helvetica, sans-serif;" +
+    "   font-size: 14px;\n" +
+    "}\n" +
+    "path.link {\n" +
+    "    fill: none;\n" +
+    "    stroke: #cfcfcf;\n" +
+    "    stroke-width: 1.5px;\n" +
+    "}";
