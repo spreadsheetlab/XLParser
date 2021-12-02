070b0dcdeda4cb494a842bf563d5f960a1300b65
Willem-Jan Meerkerk <willemjan@perfectxl.com>
2021-12-02 09:38:23 +0100

    Fix: operator precedence of plus and min

--- src/XLParser/ExcelFormulaGrammar.cs
+++ src/XLParser/ExcelFormulaGrammar.cs
@@ -452,7 +452,7 @@ namespace XLParser
             RegisterOperators(Precedence.Multiplication, Associativity.Left, mulop, divop);
             RegisterOperators(Precedence.Exponentiation, Associativity.Left, expop);
             RegisterOperators(Precedence.UnaryPostFix, Associativity.Left, percentop, hash);
-            RegisterOperators(Precedence.UnaryPreFix, Associativity.Left, plusop, minop, at);
+            RegisterOperators(Precedence.UnaryPreFix, Associativity.Left, at);
             RegisterOperators(Precedence.Union, Associativity.Left, comma);
             RegisterOperators(Precedence.Intersection, Associativity.Left, intersectop);
             RegisterOperators(Precedence.Range, Associativity.Left, colon);
