358183bb8138697b43154b50af6dfa65d34ed9c5
David Rasch <dcrasch@gmail.com>
2022-05-19 14:14:52 +0200

    add round brackets for url paths and documents

--- src/XLParser/ExcelFormulaGrammar.cs
+++ src/XLParser/ExcelFormulaGrammar.cs
@@ -185,7 +185,7 @@ namespace XLParser
         
         // Source: http://stackoverflow.com/a/6416209/572635
         private const string windowsFilePathRegex = @"(?:[a-zA-Z]:|\\?\\?[\w\-.$ ]+)\\(([^<>:\""/\|?*\\]| )+\\)*";
-        private const string urlPathRegex = @"http(s?)\:\/\/[0-9a-zA-Z]([-.\w]*[0-9a-zA-Z])*(:(0-9)*)*[/]([a-zA-Z0-9\-\.\?\,\'+&%\$#_ ]*[/])*";
+        private const string urlPathRegex = @"http(s?)\:\/\/[0-9a-zA-Z]([-.\w]*[0-9a-zA-Z])*(:(0-9)*)*[/]([a-zA-Z0-9\-\.\?\,\'+&%\$#_ ()]*[/])*";
         private const string filePathRegex = @"(" + windowsFilePathRegex + @"|" + urlPathRegex + @")";
         public Terminal FilePathToken { get; } = new RegexBasedTerminal(GrammarNames.TokenFilePath, filePathRegex)
         { Priority = TerminalPriority.FileNamePath };
