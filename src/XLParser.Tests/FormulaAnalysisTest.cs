c62c44f7df16823d2e70021870501b069f1b5c36
David <david.van.deursen@gmail.com>
2025-01-07 13:54:25 +0100

    Added test and added tilde as allowed character in server name

--- src/XLParser.Tests/FormulaAnalysisTest.cs
+++ src/XLParser.Tests/FormulaAnalysisTest.cs
@@ -613,6 +613,18 @@ namespace XLParser.Tests
         }
 
         [TestMethod]
+        public void ExternalWorkbookWithTilde()
+        {
+            //See [#205] (https://github.com/spreadsheetlab/XLParser/issues/205)
+            List<ParserReference> references = new FormulaAnalyzer(@"='\\TEST-~01\Folder\[Book1.xlsx]Sheet1'!$A$1").ParserReferences().ToList();
+
+            Assert.AreEqual(1, references.Count);
+            Assert.AreEqual(@"\\TEST-~01\Folder\", references.First().FilePath);
+            Assert.AreEqual("Book1.xlsx", references.First().FileName);
+            Assert.AreEqual("Sheet1", references.First().Worksheet);
+        }
+
+        [TestMethod]
         public void ExternalWorkbookUrlPathHttp()
         {
             List<ParserReference> references = new FormulaAnalyzer(@"='http://example.com/test/[Book1.xlsx]Sheet1'!$A$1").ParserReferences().ToList();
